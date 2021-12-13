5ed630e32e2cb0a31d23e02c1d317333516d0cd5
MarchukVyacheslav <marchukvyacheslav@bk.ru>
2021-12-13 16:49:14 +0300

    create dependency injection container

---
+++ DependencyInjectionContainer/DependencyInjection/ImplementationInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DependencyInjection
+{
+    public enum LifeTime
+    {
+        Singleton, /*на все запросы зависимостей возвращается один экземпляр объекта*/
+
+        Instance   /*каждый новый запрос зависимости из контейнера приводит к созданию нового объекта*/
+    }
+
+    public class ImplementationInfo
+    {
+        internal Type ImplementationType;
+        internal LifeTime LifeTime;
+
+        public ImplementationInfo(Type _implementationType, LifeTime _lifeTime)
+        {
+            ImplementationType = _implementationType;
+            LifeTime = _lifeTime;
+        }
+    }
+}
